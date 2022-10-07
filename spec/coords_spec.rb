a7d004bf8f8a7f227f0eefb64c68d1dfbd591145
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-07 16:09:02 -0700

    testing for node class returning coords

---
+++ spec/coords_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: false
+
+require './lib/coords'
+
+describe Node do
+  describe '#coords' do
+    subject(:placed_node) { described_class.new(3, 5) }
+
+    it 'returns an object with x coords' do
+      coords = placed_node.coords
+      x = coords.x
+      expect(x).to eq(3)
+    end
+
+    it 'returns an object with y coords' do
+      coords = placed_node.coords
+      y = coords.y
+      expect(y).to eq(5)
+    end
+  end
+end
