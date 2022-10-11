184f575afe0bf576d15b6377cda2312bbdd66e86
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-10 18:00:57 -0700

    test for node's ability to track for ownership

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: false
 
 require './lib/node'
+require './lib/player'
 
 describe Node do
   describe '#coords' do
@@ -16,6 +17,21 @@ describe Node do
       coords = placed_node.coords
       y = coords.y
       expect(y).to eq(5)
+    end
+  end
+  describe '#mark_claimed' do
+    subject(:unowned_node) { described_class.new(3, 5) }
+    let(:player) { double(Player) }
+
+    it 'is initially owned by nil' do
+      owner = unowned_node.owner
+      expect(owner).to be_nil
+    end
+
+    it 'changes owner' do
+      unowned_node.mark_claimed(player)
+      owner = unowned_node.owner
+      expect(owner).to equal(player)
     end
   end
 end
