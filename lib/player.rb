c382977a849cf9f5d8aae8611190c6a23ad7ac4a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-10 17:50:26 -0700

    added id

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: false
 
+require 'securerandom'
+
 # Stores basic player info
 class Player
-  attr_reader :name, :color
+  attr_reader :name, :color, :id
 
   def initialize(name, color)
     @name = name
     @color = color
+    @id = gen_id
+  end
+
+  private
+
+  def gen_id
+    SecureRandom.uuid
   end
 end
