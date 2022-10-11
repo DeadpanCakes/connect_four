808d2dd4c251ff9a71264a842a45a71c97a5ea2f
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-10 18:03:49 -0700

    added owner and marked_claimed methods

--- lib/node.rb
+++ lib/node.rb
@@ -4,9 +4,14 @@ require './lib/coords'
 
 # Stores info of position and owner
 class Node
-  attr_reader :coords
+  attr_reader :coords, :owner
 
   def initialize(x, y)
     @coords = Coords.new(x, y)
+    @owner = nil
+  end
+
+  def mark_claimed(player)
+    @owner = player
   end
 end
