4094187374656d59c52e1c290e3711b4817d7c7a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-13 13:14:22 -0700

    added adjacent class vars

--- lib/node.rb
+++ lib/node.rb
@@ -5,10 +5,15 @@ require './lib/coords'
 # Stores info of position and owner
 class Node
   attr_reader :coords, :owner
+  attr_accessor :above, :below, :left, :right
 
   def initialize(x, y)
     @coords = Coords.new(x, y)
     @owner = nil
+    @above = nil
+    @below = nil
+    @left = nil
+    @right = nil
   end
 
   def mark_claimed(player)
