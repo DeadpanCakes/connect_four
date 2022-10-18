bfd2b080cbb8ac52098cc05825386eca37b005c6
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-18 16:27:29 -0700

    testing that game calls Grid.new once

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,16 @@
 
 require './lib/game'
 require './lib/player'
+require './lib/grid'
 
 describe Game do
   describe '#initialize' do
     it 'calls Player.new twice' do
       expect(Player).to receive(:new).twice
+      Game.new
+    end
+    it 'calls Grid.new once' do
+      expect(Grid).to receive(:new).once
       Game.new
     end
   end
