d4bf033f30e681273d8d3d2f7f93e11f58d0c165
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-21 14:33:24 -0700

    made find_victor method

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,8 @@ require './lib/grid'
 
 # Sets up Game and managaes flow
 class Game
+  attr_reader :grid
+
   def initialize
     @players = []
     populate_players
@@ -12,7 +14,30 @@ class Game
     @turn = 1
   end
 
+  def find_victor
+    graphed_nodes = @grid.gen_graph
+    claimed_nodes = graphed_nodes.reject { |node| node.owner.nil? }
+    winning_node = claimed_nodes.find { |node| find_four_horizontal(node) }
+    return winning_node.owner if winning_node
+  end
+
   private
+
+  def find_four_horizontal(node)
+    nodes = row(node)
+
+    node if nodes.length == 4 && nodes.all? { |n| n.owner == node.owner }
+  end
+
+  def row(node)
+    curr_node = node
+    nodes = [node]
+    until curr_node.right.nil? || nodes.length == 4
+      curr_node = curr_node.right
+      nodes << curr_node
+    end
+    nodes
+  end
 
   def populate_players
     puts 'Player 1'
