5d1e4e21fc8f548f3d82736bb6763712f4eac9e8
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-21 14:29:21 -0700

    testing for #find_victor method ability to find horizontal victories

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,4 +15,29 @@ describe Game do
       Game.new
     end
   end
+  describe '#find_victor' do
+    context 'in an incomplete match' do
+      subject(:incomplete_match) { described_class.new }
+      it 'returns nil' do
+        victor = incomplete_match.find_victor
+        expect(victor).to be_nil
+      end
+    end
+    context 'in a complete match' do
+      subject(:complete_match) { described_class.new }
+      let(:winner) { double(Player) }
+      it 'returns the winner when 4 nodes match horizontally' do
+        first = complete_match.grid.claimable_nodes[0]
+        second = complete_match.grid.claimable_nodes[1]
+        third = complete_match.grid.claimable_nodes[2]
+        fourth = complete_match.grid.claimable_nodes[3]
+        complete_match.grid.place_token(first, winner)
+        complete_match.grid.place_token(second, winner)
+        complete_match.grid.place_token(third, winner)
+        complete_match.grid.place_token(fourth, winner)
+        victor = complete_match.find_victor
+        expect(victor).to be winner
+      end
+    end
+  end
 end
