69c6c5b01680820a15d5bd41ea9b74f19b24c2a8
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-10-21 14:52:53 -0700

    added find_four_vertical method

--- lib/game.rb
+++ lib/game.rb
@@ -17,11 +17,17 @@ class Game
   def find_victor
     graphed_nodes = @grid.gen_graph
     claimed_nodes = graphed_nodes.reject { |node| node.owner.nil? }
-    winning_node = claimed_nodes.find { |node| find_four_horizontal(node) }
+    winning_node = claimed_nodes.find { |node| find_four_horizontal(node) || find_four_vertical(node) }
     return winning_node.owner if winning_node
   end
 
   private
+
+  def find_four_vertical(node)
+    nodes = column(node)
+
+    node if nodes.length == 4 && nodes.all? { |n| n.owner == node.owner }
+  end
 
   def find_four_horizontal(node)
     nodes = row(node)
@@ -34,6 +40,16 @@ class Game
     nodes = [node]
     until curr_node.right.nil? || nodes.length == 4
       curr_node = curr_node.right
+      nodes << curr_node
+    end
+    nodes
+  end
+
+  def column(node)
+    curr_node = node
+    nodes = [node]
+    until curr_node.below.nil? || nodes.length == 4
+      curr_node = curr_node.below
       nodes << curr_node
     end
     nodes
